33524e5aab3f913ace6710ede32bd2ad1c357656
henilp105 <37398093+henilp105@users.noreply.github.com>
2022-07-28 16:34:50 +0000

    Deploying to gh-pages from @ henilp105/webpage@35d6fa45f4bb27e5e695e06228226d6a4eb03055 🚀

--- zh_CN/_static/documentation_options.js
+++ zh_CN/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '1.0.0',
-    LANGUAGE: 'zh_CN',
+    LANGUAGE: 'zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
