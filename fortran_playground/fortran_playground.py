f5c9daa5f4dac68521a3a06c44b75980e028be78
Henil Panchal <henilp105@gmail.com>
2022-10-08 09:57:53 +0530

    check for runnable code

--- fortran_playground/fortran_playground.py
+++ fortran_playground/fortran_playground.py
@@ -9,9 +9,10 @@ url = "https://play-api.fortran-lang.org/run"
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 headers["Content-Type"] = "application/json"
-data = """
-{"code":"%s","programInput":"","libs":[]}
-"""
+data = """{"code":"%s","programInput":"","libs":[]}"""
+comp_error = [b"<ERROR>",b"Error",b"app/main.f90",b"<h1>Bad Request</h1>"]
+
+
 class PlayCodeBlock(CodeBlock):
 
     def run(self):
@@ -24,13 +25,15 @@ class PlayCodeBlock(CodeBlock):
                 literal = container_wrapper(self, literal, caption)
             except ValueError as exc:
                 return [document.reporter.warning(exc, line=self.lineno)]
-        
         resp = requests.post(url, headers=headers, data=data%clean(text))
-        print(resp.content)
-        if "<ERROR>" not in resp.content:
-            return [literal]
-        else:
+        #print(resp.content)
+        #print([i in resp.content  for i in comp_error])
+        if any(i in resp.content  for i in comp_error):
+            #print("original")
             return [*super().run()]
+        else:
+            #print("with link")
+            return [literal]
         
 
 
