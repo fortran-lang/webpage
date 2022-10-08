c735d565dcfe175caa789a4916d71273ac5b303e
Henil Panchal <henilp105@gmail.com>
2022-10-08 09:33:26 +0530

    run only specific codebloks

--- fortran_playground/fortran_playground.py
+++ fortran_playground/fortran_playground.py
@@ -1,8 +1,17 @@
 from sphinx.directives.code import *
 import urllib.parse
+import requests
+from requests.structures import CaseInsensitiveDict
 from docutils import nodes
 
+url = "https://play-api.fortran-lang.org/run"
 
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+headers["Content-Type"] = "application/json"
+data = """
+{"code":"%s","programInput":"","libs":[]}
+"""
 class PlayCodeBlock(CodeBlock):
 
     def run(self):
@@ -15,7 +24,13 @@ class PlayCodeBlock(CodeBlock):
                 literal = container_wrapper(self, literal, caption)
             except ValueError as exc:
                 return [document.reporter.warning(exc, line=self.lineno)]
-        return [literal]
+        
+        resp = requests.post(url, headers=headers, data=data%clean(text))
+        print(resp.content)
+        if "<ERROR>" not in resp.content:
+            return [literal]
+        else:
+            return [*super().run()]
         
 
 
@@ -27,3 +42,8 @@ def setup(app):
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
+
+def clean(s):
+    s = s.replace("\t", "\\t")
+    s = s.replace("\n", "\\n")
+    return s
