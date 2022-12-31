2ff7608b13bec7d1ff4a67780ec8dc0aafe90f23
Henil Panchal <henilp105@gmail.com>
2022-12-31 09:28:57 +0530

    add sitemap.xml

--- source/conf.py
+++ source/conf.py
@@ -68,6 +68,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx.ext.intersphinx",
     "sphinx_jinja",
+    "sphinx_sitemap",
 ]
 
 myst_enable_extensions = [
@@ -194,6 +195,7 @@ fontawesome_link_cdn = True
 blog_path = "news"
 blog_post_pattern = "news/**"
 blog_baseurl = "https://fortran-lang.org/en/"
+html_baseurl = "https://fortran-lang.org/"
 post_redirect_refresh = 1
 post_auto_image = 1
 post_auto_excerpt = 2
